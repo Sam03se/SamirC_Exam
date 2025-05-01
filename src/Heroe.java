af8ccc1b8cbcb93e0068e0fc465f572513912864
Examen <samir.cuenca@udla.edu.ec>
2025-05-01 14:32:01 -0500

    clase heroe completar

--- src/Heroe.java
+++ src/Heroe.java
@@ -1,5 +1,36 @@
 import java.util.*;
 
 public class Heroe {
+    private String nombre;
+    private String raza;
+    private String habilidad;
+    private int nivelPoder;
+
+    public Heroe(int nivelPoder, String nombre, String raza, String habilidad){
+        this.nivelPoder= nivelPoder;
+        this.habilidad= habilidad;
+        this.raza=raza;
+        this.nombre=nombre;
+
+    }
+
+    public int getNivelPoder(){
+        return nivelPoder;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+    public void mostrar(){
+        System.out.println("El nvel de poder es:"+nivelPoder);
+    }
 
 }
