f4406d340d06182c302bace1c39db9644de667f6
Examen <samir.cuenca@udla.edu.ec>
2025-05-01 14:53:13 -0500

    clase cola heroe

---
+++ src/ColaHeroe.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ColaHeroe {
+    private Queue<Heroe> cola;
+
+    public ColaHeroe() {
+        cola = new LinkedList<>();
+    }
+
+    public void agregarHeroe(Scanner scanner) {
+        System.out.print("Ingrese el nivel de poder del Heroe");
+        int nivelPoder = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Ingrese el nombre del Heroe");
+        String nombre = scanner.nextLine();
+        System.out.print("Ingrese la raza del Heroe( Mutante, Humano, Alien");
+        String raza = scanner.nextLine();
+        System.out.print("Ingrese la habilidad del Heroe (Telepatia, Superfuerza, Vuelo");
+        String habilidad = scanner.nextLine();
+
+        Heroe nuevo = new Heroe(nivelPoder, nombre, raza, habilidad);
+        cola.offer(nuevo);
+        System.out.println("Heroe agregado a la cola");
+    }
+
+    public void verPrimero(){
+        Heroe primero=cola.peek();
+        if(primero != null){
+            System.out.println("El primer heroe de la cola es:");
+            primero.mostrar();
+
+        }else {
+            System.out.println("La cola esta vacia:");
+        }
+    }
+}
+
