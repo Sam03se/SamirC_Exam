8380087950df13d71ce930dff9e777adf5d1726b
Examen <samir.cuenca@udla.edu.ec>
2025-05-01 15:12:19 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner=new Scanner(System.in);
+        ColaHeroe control= new ColaHeroe();
+        int opcion;
+
+        do {
+            System.out.println("\n--------------------HEROES--------------");
+            System.out.println("1.- Añadir un heroe");
+            System.out.println("2.- Ver Primer Heroe en la cola");
+            System.out.println("3.- Eliminar heroe ");
+            System.out.println("4.- Mostrar todos los heroes");
+            System.out.println("5.- Salir");
+            System.out.println("Seleccione una opcion");
+            opcion=scanner.nextInt();
+        }
     }
 }
